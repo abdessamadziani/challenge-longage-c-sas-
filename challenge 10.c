8d30ad7abee86c6168cc18831cdfa76bdf337149
abdessamd <zianiabdssamad30@gmail.com>
2021-11-11 10:55:59 +0100

    challenge 10

---
+++ challenge 10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    printf("*********** la circonference d'un cercle*************\n");
+const float pi=3.14;
+float r,circonference;
+printf("Veullez entrer le Rayon : \n");
+scanf("%f",&r);
+circonference=2*pi*r;
+printf("la circonférence d'un cette cercle est : %.2f",circonference);
+
+}

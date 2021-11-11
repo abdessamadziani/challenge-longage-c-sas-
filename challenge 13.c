f720bfacb3f71b1ce76eddaf2cf4eae5930860b9
abdessamd <zianiabdssamad30@gmail.com>
2021-11-11 14:08:39 +0100

    challenge 13

---
+++ challenge 13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main ()
+{
+    printf("************Affichage de la valeur octale et hexadecimale equivalente***************\n");
+    int nb;
+    printf("Veuillez donner un nombre :\n");
+    scanf("%d",&nb);
+    printf(" la convertation de %d en octal est : %o\n",nb,nb);
+    printf(" la convertation de %d en hexadecimale est : %x\n",nb,nb);
+
+
+}
